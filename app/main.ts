db72511fc544722de1335fcd9a3dead9068de3da
zwiens <zwiens@sfu.ca>
2017-08-30 08:52:47 -0700

    COMMIT MESSAGE HERE

---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by funon on 8/28/2017.
+ */
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {    AppModule} from "./app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule);
